fc0c14533c345cf6e9dfc1cf43277283598a7172
axect <edeftg@gmail.com>
2018-12-25 04:31:02 +0900

    So Hard

--- src/tov/mod.rs
+++ src/tov/mod.rs
@@ -1,1 +1,3 @@
+pub mod ode;
 pub mod rk4;
+pub mod gl4;
--- src/tov/rk4.rs
+++ src/tov/rk4.rs
@@ -1,26 +1,12 @@
 extern crate peroxide;
 use peroxide::*;
 
-#[derive(Debug)]
-pub struct ODE {
-    pub param: f64,
-    pub values: Vec<f64>,
-    pub step: f64,
-    pub records: Matrix,
-    pub stage: usize,
-    pub num: usize,
-}
+use crate::tov::ode::ODE;
 
 pub trait ERK4 {
     fn new(init_param: f64, init_val: Vec<f64>, step: f64, num: usize) -> ODE;
     fn update<F>(&mut self, f: &F) where F: Fn(f64, Vec<f64>) -> Vec<f64>;
     fn integrate<F>(&mut self, f: F) where F: Fn(f64, Vec<f64>) -> Vec<f64>;
-}
-
-pub trait GL4 {
-    fn gl_new(init_param: f64, init_val: Vec<f64>, step: f64, num: usize) -> ODE;
-    fn gl_update<F>(&mut self, f: &F) where F: Fn(f64, Vec<f64>) -> Vec<f64>;
-    fn gl_integrate<F>(&mut self, f: F) where F: Fn(f64, Vec<f64>) -> Vec<f64>;
 }
 
 impl ERK4 for ODE {
@@ -91,49 +77,3 @@ impl ERK4 for ODE {
         }
     }
 }
-
-impl GL4 for ODE {
-    fn gl_new(init_param: f64, init_val: Vec<f64>, step: f64, num: usize) -> ODE {
-        let l = init_val.len();
-
-        let v1_temp1 = vec![init_param]; // len = 1
-        let v1_temp2 = init_val.clone(); // len = l
-        let v1 = c!(v1_temp1; v1_temp2);
-        let v2 = vec![0f64; (l+1) * (num)];
-        ODE {
-            param: init_param,
-            values: init_val,
-            step: step,
-            records: matrix(
-                c!(v1; v2),
-                num+1, // Original Value
-                l+1, // param + ys
-                Row,
-            ),
-            stage: 0,
-            num: num,
-        }
-    }
-
-    fn gl_update<F>(&mut self, f: &F) where F: Fn(f64, Vec<f64>) -> Vec<f64> {
-        let t = self.param;
-        let ys = self.values.clone();
-        let h = self.step;
-
-        let mnum_11: f64 = 0.5 - 3f64.sqrt()/6f64;
-        let mnum_12: f64 = 0.25 - 3f64.sqrt()/6f64;
-        let mnum_21: f64 = 0.5 + 3f64.sqrt()/6f64;
-        let mnum_22: f64 = 0.25 + 3f64.sqrt()/6f64;
-
-        let t_1 = t + mnum_11 * h;
-        let t_2 = t + mnum_21 * h;
-
-
-        unimplemented!()
-    }
-
-    fn gl_integrate<F>(&mut self, f: F) where F: Fn(f64, Vec<f64>) -> Vec<f64> {
-        unimplemented!()
-    }
-}
-
