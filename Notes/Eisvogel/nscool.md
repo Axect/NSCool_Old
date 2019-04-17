464869f3d536f792f71d4fa4ddfa30da991eb8de
axect <axect@outlook.kr>
2019-04-17 16:26:14 +0900

    Schwarzschild Metric Master

---
+++ Notes/Eisvogel/nscool.md
@@ -0,0 +1,18 @@
+---
+fontfamily: "libertine"
+mainfont: "GFS Artemisa"
+title: "Neutron Star Cooling"
+author: [Tae Geun Kim]
+date: 2019-04-16
+subject: "Markdown"
+keywords: [Markdown, Example]
+subtitle: "With BSk EoS & Rust"
+titlepage: true
+toc-own-page: true
+...
+
+\newpage\null\thispagestyle{empty}\newpage
+
+# Structure
+
+
--- src/tov/eos.rs
+++ src/tov/eos.rs
@@ -5,14 +5,6 @@ use std::f64::consts::PI;
 use crate::consts::consts::*;
 pub use self::EOSModel::*;
 
-// ===========================================================
-// Declare Constants
-// ===========================================================
-pub const K: f64 = 300.;
-pub const GAMMA: usize = 2;
-pub const GAMMAF: f64 = 2.;
-pub const RHO0C: f64 = 0.4;
-
 #[derive(Debug, Clone)]
 pub enum EOSModel {
     BSk19,
@@ -43,70 +35,6 @@ pub fn log_p_of_log_rho(xi: f64, model: EOSModel) -> f64 {
     + (a[13] + a[14]*xi) / ((a[15] * (a[16] - xi)).exp() + 1f64)
     + a[17] / (1f64 + (a[18] * (xi - a[19])).powi(2))
     + a[20] / (1f64 + (a[21] * (xi - a[22])).powi(2))
-}
-
-
-
-/// Tolman-Oppenheimer-Volkoff Equations for Spherically Symmetric Equilibrium Stars
-///
-/// # Equation
-/// ```latex
-/// dm/dr = 4πr^2ρ
-/// dP/dr = -ρm/r^2 (1 + P/ρ) (1 + 4πPr^3/m) (1 - 2m/r)^{-1}
-/// dΦ/dr = -1/ρ dP/dr (1 + P/ρ)^{-1}
-/// P = Kρ_0^Γ
-/// Γ = 1 + 1/n
-/// ρ = ρ_0(1 + ε)
-/// m = 0, P = P_c at r = 0
-/// ```
-pub fn tov_rhs(r: Dual, rs: Vec<Dual>) -> Vec<Dual> {
-    let m_old = rs[0];
-    let _phi_old = rs[1];
-    let p_old = rs[2];
-
-    let rho_0 = (p_old / K).powf(1. / GAMMAF);
-    let rho_old = rho_0 + p_old / (GAMMAF - 1f64);
-
-    let dm = 4f64 * PI * r.pow(2) * rho_old;
-
-    let dphi = if r.value() != 0f64 { (m_old + 4f64 * PI * r.pow(3) * p_old) / (r.pow(2) * (1f64 - 2f64 * m_old / r)) } else { dual(0, 0) };
-    let dp = if r.value() != 0f64 {- (rho_old + p_old) * (m_old + 4f64 * PI * r.pow(3) * p_old) / (r.pow(2) * (1f64 - 2f64 * m_old / r)) } else { dual(0, 0) };
-
-    vec![dm, dphi, dp]
-}
-
-/// Import eos
-///
-/// # Column (Each units are CGS)
-///
-///   Rho        Press         nbar        Ye         Ymu        Yn
-/// [g/cm3]   [dyne/cm2]     [#/fm3]     [A_cell]    [A_ion]     [Z]
-pub fn import_eos(title: &str, unit: UnitSystem) -> (Vec<f64>, Vec<f64>, Vec<f64>) {
-    let m = Matrix::read(title, false, ' ').expect("Can't read file");
-    let rho_eos = m.col(0);
-    let p_eos = m.col(1);
-    let nbar_eos = m.col(2);
-
-    match unit {
-        UnitSystem::CGS => {
-            (rho_eos, p_eos, nbar_eos)
-        },
-        UnitSystem::Geometrized => {
-            (
-                rho_eos.fmap(|t| cgs_to_geom(t, Density)),
-                p_eos.fmap(|t| cgs_to_geom(t, Pressure)),
-                nbar_eos
-            )
-        },
-        UnitSystem::Natural => {
-            (
-                rho_eos.fmap(|t| cgs_to_natural(t, Density)),
-                p_eos.fmap(|t| cgs_to_natural(t, Pressure)),
-                nbar_eos
-            )
-        }
-    }
-
 }
 
 pub const PARAM_BSk19: [f64; 23] = [
