843c78e2f10220c6b0fa435fda6956880e945b70
axect <axect@outlook.kr>
2019-09-26 14:47:13 +0900

    Piece 4

--- src/bin/eos_test.rs
+++ src/bin/eos_test.rs
@@ -5,10 +5,15 @@ use peroxide::Printable;
 use NSCool::structure::eos::*;
 use NSCool::structure::eos::EOSModel::*;
 
+use std::env;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let piece: usize = args[1].parse::<usize>().unwrap();
+
     let eos_data = load_table(SLy4);
     println!("Load table success");
-    let p = piecewise_poly_fit(&eos_data, 3);
+    let p = piecewise_poly_fit(&eos_data, piece);
     let kgs = p.extract_k_gamma();
 
     println!("{:?}", kgs);
